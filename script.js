a605c6e1675758df8da9049204fcf35c1fcf49ed
ADELEKE TEMITAYO <lekedevrepo@gmail.com>
2019-08-12 16:46:21 -0600

    fix bugs

--- script.js
+++ script.js
@@ -2,17 +2,18 @@
 window.onscroll = function() {myFunction()};
 
 // Get the header
-//var header-list = document.getElementById("");
+//var header-list = document.getElementById("hidden-logo");
 var header = document.getElementById("main-nav");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Add the sticky class to the header when you reach its scroll position. 
+//Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
-  	//document.getElementById("hidden").style.display = "none";
     header.classList.add("sticky");
+    //header-list.classList.add("logo-list");
   } else {
     header.classList.remove("sticky");
   }
