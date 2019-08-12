7e3630ddbffebecd5afca417871db390f6777a68
ADELEKE TEMITAYO <lekedevrepo@gmail.com>
2019-08-12 15:58:37 -0600

    add hidden-list html structure

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 window.onscroll = function() {myFunction()};
 
 // Get the header
-var header = document.getElementById("myHeader");
+//var header-list = document.getElementById("");
+var header = document.getElementById("main-nav");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
@@ -10,6 +11,7 @@ var sticky = header.offsetTop;
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
+  	//document.getElementById("hidden").style.display = "none";
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
